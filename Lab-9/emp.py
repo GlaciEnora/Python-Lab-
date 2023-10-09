3b2daca4b1b3f1d11fbde35fb487208e9800a82c
Glaci Enora <142082958+GlaciEnora@users.noreply.github.com>
2023-10-09 19:56:33 +0530

    Update emp.py

--- Lab-9/emp.py
+++ Lab-9/emp.py
@@ -12,18 +12,22 @@ class Employee:
         self.designation = input("Enter employee designation: ")
         self.__salary = int(input("Enter the salary: "))
 
-    def retsal(self):
+    def get_sal(self):
         return self.__salary
     
+    def set_sal(self, a):
+        self.__salary = a
+
     def display(self):
         print("Name: ", self.name, "\nID: ", self.emp_id, "\nDesignation: ", self.designation)
 
-def calculate(emp, da, hra, pf):
-    grosssal = emp.retsal()*(1+(da+hra)/100)
-    netsal = emp.retsal()*(1+(da+hra-pf)/100)
-    print("Gross salary: ", grossal)
-    print("Net salary: ", netsal)
+    salary = property(get_sal, set_sal)
 
+def calculate(emp, da, hra, pf):
+    emp.salary *= (1+(da+hra)/100)
+    print("Gross salary: ", emp.salary)
+    emp.salary *= (1+(da+hra-pf)/100)
+    print("Net salary: ", emp.salary)
 
 obj = Employee()
 obj.getdetail()
